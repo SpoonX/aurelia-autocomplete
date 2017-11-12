556eee9b96f0f9c19b5040021fa1a97869cc6057
Sayan751 <sayan751@gmail.com>
2017-11-12 18:03:46 +0100

    Moving to on type definition folder for all

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,3 @@
-// // all default gulp tasks are located in the ./node_modules/spoonx-tools/build-plugin/tasks directory
-// // gulp default configuration is in files in ./node_modules/spoonx-tools/build-plugin directory
-// require('require-dir')('node_modules/spoonx-tools/build-plugin/tasks');
-
-// // 'gulp help' lists the available default tasks
-// // you can add additional tasks here
-// // the express server app can be imported and routes added
-// var app = require('./node_modules/spoonx-tools/build-plugin/tasks/server').app;
-
-// // default: all routes, all methods
-// app.all('*', function (req, res) {
-//   res.send({
-//     path: req.path,
-//     query: req.query,
-//     body: req.body,
-//     method: req.method,
-//     contentType: req.header('content-type'),
-//     Authorization: req.header('Authorization'),
-//     originalUrl: req.originalUrl,
-//     access_token: req.body.access_token,
-//     refresh_token: req.body.refresh_token
-//   });
-// });
-
-/*--------------------------------------*/
-
 const compileToModules = ['es2015', 'commonjs', 'amd', 'system'];
 const base = "./";
 const paths = {
@@ -55,15 +29,21 @@ compileToModules.forEach(function (moduleType) {
   // if (moduleType === 'native-modules') return; // typescript doesn't support the combination of: es5 + native modules
 
   gulp.task(tsTask, function () {
+    if (moduleType === 'native-modules')
+      return srcForBabel()
+        .pipe(to5(assign({}, compilerOptions[moduleType]())))
+        .pipe(cleanGeneratedCode())
+        .pipe(gulp.dest(paths.output + moduleType));
+
     const tsProject = ts.createProject(paths.tsConfig, {
       module: moduleType,
       target: moduleType == 'es2015' ? 'es2015' : 'es5'
     });
-    
+
     const tsResult = tsProject.src().pipe(tsProject());
 
     return merge([
-      tsResult.dts.pipe(gulp.dest(paths.dist + moduleType + '/definitions')),
+      tsResult.dts.pipe(gulp.dest(paths.dist + '/definitions')),
       tsResult.js.pipe(
         gulp.dest(paths.dist + moduleType)
       )
